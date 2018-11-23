82033c48582a4700dfbc7a90825d96a4f20c0f03
CodeAsteriatedGrail <ym950531@gmail.com>
2018-11-23 21:20:25 +0800

    add calculator

---
+++ js/cal.js
@@ -0,0 +1,8 @@
+/*document.getElementsByType('button').addEventListener("click", myFunction);*/
+
+function myFunction(num) {
+	
+   document.getElementById("demo").innerHTML =num;
+   
+	/*alert(num);*/
+}
