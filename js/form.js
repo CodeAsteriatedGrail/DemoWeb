db4ba35c944de5daba82130ab4c6437c3912ec3a
CodeAsteriatedGrail <ym950531@gmail.com>
2018-11-28 21:27:01 +0800

    debug

--- js/form.js
+++ js/form.js
@@ -1,12 +1,13 @@
 function myFunction() {
 	var y = document.getElementById("que-2").value;
     var x = document.getElementById("que-1").value;
-	var rdbtn = document.getElementById("yes");
-	var result=document.getElementById("demo");
-	if(rdbtn.checked = true){
-		result.innerHTML = "YES，理由："+x+"在"+y+"上班";}
-	else{
-		result.innerHTML = "NO，理由："+x+"，目前在"+y+"上班";
+	var rdbtn = document.querySelector('input[name="yn"]:checked').value;
+	if(x == ""){
+		console.log(x);
+		document.getElementById("ned-1").style.backgroundColor = "red";
 	}
+	
+	console.log(rdbtn);
+	console.log("理由"+y+"，目前在"+x+"上班");	
     
 }
